77417219f4223a7f010a77135c3af79bc4e0cd49
Silya1993 <silyaak93@gmail.com>
2022-12-13 12:16:40 -0500

    first commit

---
+++ src/main/java/com/maven/remotenew/Remote_NEW_NW/App.java
@@ -0,0 +1,13 @@
+package com.maven.remotenew.Remote_NEW_NW;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
